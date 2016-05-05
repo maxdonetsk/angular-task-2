e6bf8a0c77f7d4307b528e77814ef0bd03e35996
maxdonetsk <max.donetsk@mail.ru>
2016-05-05 18:11:17 +0300

    first part is done with bonus tasks

--- js/controllers/Users.js
+++ js/controllers/Users.js
@@ -3,7 +3,13 @@
 
     angular
             .module('app')
-            .controller('Users', function ($scope) {
-
+            .controller('Users', function ($scope, Users) {
+                $scope.showEditInput = 0;
+                Users.getUsers().then(function (data) {
+                    $scope.users = data;
+                });
+                $scope.deleteUser = Users.deleteUser;
+                $scope.editUser = Users.editUser;
+                $scope.addUser = Users.addUser;
             });
 }());
--- js/services/Users.js
+++ js/services/Users.js
@@ -4,8 +4,36 @@
     angular
             .module('app')
             .factory('Users', function ($http) {
-                    return {
-                        
-                    };
-                });
+                var users = [];
+                function getUsers() {
+                    return $http.get('../data/users.json').then(function (result) {
+                        users = result.data;
+                        return users;
+                    });
+                }
+
+                function deleteUser(user) {
+                    users.splice(users[user.id - 1], 1);
+                }
+
+                function editUser(user) {
+                    return angular.copy(user);
+                }
+                
+                function addUser(user) {
+                    user = {
+                        id: users.length + 1,
+                        username: user.username,
+                        permissions: user.permissions
+                    }
+                    users.push(user);
+                }
+
+                return  {
+                    getUsers: getUsers,
+                    deleteUser: deleteUser,
+                    editUser: editUser,
+                    addUser: addUser
+                };
+            });
 })();
